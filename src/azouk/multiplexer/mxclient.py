87a0fa1a2cdd5308d569d379b0dd2f89a46734b8
piotr.findeisen <piotr.findeisen@0fdfa6c8-de5c-11dd-939f-4b74b2738814>
2009-04-17 20:40:41 +0000

    simplify mxclient internal interfaces + format

--- src/azouk/multiplexer/mxclient.py
+++ src/azouk/multiplexer/mxclient.py
@@ -30,6 +30,7 @@ from types import MethodType
 
 from multiplexer.protocolbuffers import *
 from multiplexer.multiplexer_constants import peers, types
+from azouk.logging import log, WARNING, HIGHVERBOSITY
 
 def set_if_missing(d, k, v):
     """assign key `k' in the dictionary `d' if it's not assigned"""
@@ -58,7 +59,8 @@ def initialize_message(_message, **kwargs):
 
 def make_message(_type, **kwargs):
     """
-        make_message(MessageType, **kwargs) -> instance of MessageType with attributes set
+        make_message(MessageType, **kwargs) -> instance of MessageType with
+        attributes set
         Create a message using factory function type and
         assign its attributes according do kwargs (attribute, value).
     """
@@ -66,7 +68,7 @@ def make_message(_type, **kwargs):
     initialize_message(message, **kwargs)
     return message
 
-def dict_message(m, all_fields = False, recursive = False):
+def dict_message(m, all_fields=False, recursive=False):
     """
         Operation reverse to make_message.
         If all_fields == False, only set fields are provied.
@@ -82,7 +84,7 @@ def dict_message(m, all_fields = False, recursive = False):
     if recursive:
         for k, v in d.items():
             if isinstance(v, google.protobuf.message.Message):
-                d[k] = dict_message(v, all_fields = all_fields, recursive = True)
+                d[k] = dict_message(v, all_fields=all_fields, recursive=True)
 
     return d
 
@@ -138,7 +140,7 @@ class Client(_mxclient.Client):
         ip4 = socket.gethostbyname(endpoint[0])
         return super(Client, self).async_connect(ip4, endpoint[1])
 
-    def connect(self, endpoint, timeout = DEFAULT_TIMEOUT):
+    def connect(self, endpoint, timeout=DEFAULT_TIMEOUT):
         """
             synchronous connect
             endpoint is e.g. ("localhost", 1980)
@@ -147,11 +149,11 @@ class Client(_mxclient.Client):
         ip4 = socket.gethostbyname(endpoint[0])
         return super(Client, self).connect(ip4, endpoint[1], timeout)
 
-    def wait_for_connection(self, connwrap, timeout = DEFAULT_TIMEOUT):
+    def wait_for_connection(self, connwrap, timeout=DEFAULT_TIMEOUT):
         """wait for connection initiated with async_connect"""
         return super(Client, self).wait_for_connection(connwrap, timeout)
 
-    def __receive_message(self, timeout = -1):
+    def __receive_message(self, timeout=-1):
         """
             blocking read from all the sockets (or from incoming message queue)
             returns (MultiplexerMessage, ConnectionWrapper)
@@ -161,17 +163,19 @@ class Client(_mxclient.Client):
         mxmsg = parse_message(MultiplexerMessage, next[0])
         return (mxmsg, next[1])
 
-    def receive_message(self, timeout = -1):
+    def receive_message(self, timeout=-1):
         return self.__receive_message(timeout)
 
-    def handle_drop(self, mxmsg, connwrap = None):
-        """overide in subclass if you want to get hold on every message dropped"""
-        print >> sys.stderr, "dropping message %r" % dict(
-                [ (k, getattr(mxmsg, k)) for k in ['id', 'type', 'to', 'from', 'references'] ] + \
-                [ ('len', len(mxmsg.message)) ]
-                )
+    def handle_drop(self, mxmsg, connwrap=None):
+        """overide in subclass if you want to get hold on every message
+           dropped"""
+        log(WARNING, HIGHVERBOSITY,
+                text="dropping message %r" % dict(id=mxmsg.id, type=mxmsg.type,
+                    to=mxmsg.to, from_=mxmsg.from_,
+                    references=mxmsg.references, len=len(mxmsg.message))
+            )
 
-    def _check_type(self, message, type, exc = OperationFailed):
+    def _check_type(self, message, type, exc=OperationFailed):
         assert isinstance(message, MultiplexerMessage)
         if message.type != type:
             raise exc()
@@ -180,25 +184,28 @@ class Client(_mxclient.Client):
         return parse_message(type, message)
 
     def event(self, *args, **kwargs):
-        """send message through all active MX connections (arguments same as for send_message)"""
-        return self.send_message(multiplexer = Client.ALL, *args, **kwargs)
+        """send message through all active MX connections (arguments same as
+           for send_message)"""
+        return self.send_message(multiplexer=Client.ALL, *args, **kwargs)
 
-    def query(self, message, type, timeout = DEFAULT_TIMEOUT):
+    def query(self, message, type, timeout=DEFAULT_TIMEOUT):
         """
             Reliably ask for the response.
 
-            Send a request `message' with type `type' through one MX connection.
-            If there is no response within `timeout' seconds, query all connected MX servers,
-            to find working backend that will handle the request. If such a backend is found
-            within `timeout' seconds, repeat the request directly to the working backend through
+            Send a request `message' with type `type' through one MX
+            connection.  If there is no response within `timeout' seconds,
+            query all connected MX servers, to find working backend that will
+            handle the request. If such a backend is found within `timeout'
+            seconds, repeat the request directly to the working backend through
             the working MX connection.
         """
         assert not isinstance(message, MultiplexerMessage)
-        query = self.new_message(type = type, message = message)
-        ignore_function = lambda mxmsg, connwrap: mxmsg.type == types.REQUEST_RECEIVED
+        query = self.new_message(type=type, message=message)
         first_request_delivery_errored = False
         try:
-            response, connwrap = self.send_and_receive(query, multiplexer = Client.ONE, timeout = timeout, ignore_function = ignore_function)
+            response, connwrap = self.send_and_receive(query,
+                    multiplexer=Client.ONE, timeout=timeout,
+                    ignore_types=(types.REQUEST_RECEIVED,))
             if response.type != types.DELIVERY_ERROR:
                 return response
             first_request_delivery_errored = True
@@ -208,51 +215,64 @@ class Client(_mxclient.Client):
         # find working backends
         search = BackendForPacketSearch()
         search.packet_type = type
-        mxmsg = self.new_message(message = search, type = types.BACKEND_FOR_PACKET_SEARCH)
-        response, connwrap = self.send_and_receive(mxmsg, accept_ids = [query.id], multiplexer = Client.ALL, timeout = timeout,
-                handle_delivery_errors = True, ignore_function = ignore_function)
-        
+        mxmsg = self.new_message(message=search,
+                type=types.BACKEND_FOR_PACKET_SEARCH)
+        response, connwrap = self.send_and_receive(mxmsg,
+                accept_ids=[query.id], multiplexer=Client.ALL, timeout=timeout,
+                handle_delivery_errors=True,
+                ignore_types=(types.REQUEST_RECEIVED,))
+
         if response.type == types.DELIVERY_ERROR:
             if first_request_delivery_errored:
                 raise OperationFailed
-            return self.receive(accept_ids = [query.id], timeout = timeout)
+            return self.receive(accept_ids=[query.id], timeout=timeout)
         if response.references == query.id:
             return response
 
         self._check_type(response, types.PING)
 
         # resent original query directly to the working backend
-        direct_query = self.new_message(to = response.from_, message = message, type = type)
+        direct_query = self.new_message(to=response.from_, message=message,
+                type=type)
         assert direct_query.type == type
         assert type
-        response, connwrap = self.send_and_receive(direct_query, accept_ids = [query.id], ignore_ids = [mxmsg.id], multiplexer = connwrap,
-                timeout = timeout, ignore_function = ignore_function)
+        response, connwrap = self.send_and_receive(direct_query,
+                accept_ids=[query.id], ignore_ids=[mxmsg.id],
+                multiplexer=connwrap,
+                timeout=timeout, ignore_types=(types.REQUEST_RECEIVED,))
 
         if response.type == types.DELIVERY_ERROR:
-            raise OperationTimedOut
+            raise OperationFailed
 
         return response
 
-    def send_and_receive(self, message, accept_ids = [], ignore_ids = [], timeout = DEFAULT_TIMEOUT, handle_delivery_errors = False, ignore_function = None, **kwargs):
+    def send_and_receive(self, message, accept_ids=[], ignore_ids=[],
+            timeout=DEFAULT_TIMEOUT, handle_delivery_errors=False,
+            ignore_types=(), **kwargs):
         """
             Unreliably ask for the response.
 
-            Sends a request `message' and wait until `timeout' second pass or a response to message or one of accept_ids
-            is received.
+            Sends a request `message' and wait until `timeout' second pass or a
+            response to message or one of accept_ids is received.
         """
         timeout_ticker = TimeoutTicker(timeout)
-        id, tracker = self.send_message(message, timeout = timeout_ticker(), **kwargs)
+        id, tracker = self.send_message(message, timeout=timeout_ticker(),
+                **kwargs)
         if isinstance(tracker, ScheduledMessageTracker):
-            self.flush(tracker, timeout = timeout_ticker())
+            self.flush(tracker, timeout=timeout_ticker())
         else:
             assert isinstance(tracker, int)
             assert tracker >= 0
 
         accept_ids = [id] + accept_ids
         while timeout_ticker.permit():
-            mxmsg, connwrap = self.receive(accept_ids = accept_ids, ignore_ids = ignore_ids, ignore_function = ignore_function, timeout_ticker = timeout_ticker)
-            if mxmsg.type == types.DELIVERY_ERROR and handle_delivery_errors and isinstance(tracker, int):
-                # event was sent and we don't want to stop on first DELIVERY_ERROR received
+            mxmsg, connwrap = self.receive(accept_ids=accept_ids,
+                    ignore_ids=ignore_ids, ignore_types=ignore_types,
+                    timeout_ticker=timeout_ticker)
+            if mxmsg.type == types.DELIVERY_ERROR and \
+                    handle_delivery_errors and isinstance(tracker, int):
+                # event was sent and we don't want to stop on first
+                # DELIVERY_ERROR received
                 tracker -= 1
                 if tracker > 0:
                     continue
@@ -261,16 +281,16 @@ class Client(_mxclient.Client):
         raise OperationTimedOut
 
 
-    def receive(self, accept_ids, ignore_ids = [], ignore_function = None, timeout = DEFAULT_TIMEOUT, timeout_ticker = None):
+    def receive(self, accept_ids, ignore_ids=[], ignore_types=(),
+            timeout=DEFAULT_TIMEOUT, timeout_ticker=None):
         """
             like send_and_receive but without sending anything
         """
         if timeout_ticker is None: timeout_ticker = TimeoutTicker(timeout)
-        if ignore_function is None: ignore_function = lambda *args: False
 
         while timeout_ticker.permit():
-            mxmsg, connwrap = self.__receive_message(timeout = timeout_ticker())
-            if ignore_function(mxmsg, connwrap):
+            mxmsg, connwrap = self.__receive_message(timeout=timeout_ticker())
+            if mxmsg.type in ignore_types:
                 continue
 
             if mxmsg.references in accept_ids:
@@ -278,35 +298,48 @@ class Client(_mxclient.Client):
 
             if mxmsg.references not in ignore_ids:
                 # unexpected message received
-                print >> sys.stderr, "other while waiting for reply for %r" % (accept_ids,)
+                log(WARNING, HIGHVERBOSITY,
+                    text="message (id=%d, type=%d, from=%d, references=%d) "
+                        "while waiting for reply for %r" % \
+                            (mxmsg.id, mxmsg.type, mxmsg.from_,
+                                mxmsg.references, accept_ids)
+                    )
                 self.handle_drop(mxmsg, connwrap)
 
         raise OperationTimedOut()
 
     def send_message(self, message, **kwargs):
         """
-            sends a message through one or more MX servers; usually non-blocking
+            sends a message through one or more MX servers; usually
+            non-blocking
 
-            params
-                message -- MultiplexerMessage or something that will become a message body (string, PorotocolBuffer Message)
-                    (if embed is True, message will become final message's body without checking types)
-
-            keyword-only params
-                embed -- force creating new MultiplexerMessage, don't check `message' type
-                multiplexer [ ONE | ALL | Connwrap ] -- use specific (set of) MX, default = ONE
-                flush -- wait until message is passed to the socket layer (kernel)
-                timeout -- timeout
-                **kwargs -- will be used in construting MultiplexerMessage where `embed' or message is not instance of MultiplexerMessage
             returns (message_id, message_tracker)
+
+            :Parameters:
+                - `message`: MultiplexerMessage or something that will become a
+                  message body (string, PorotocolBuffer Message) (if embed is
+                  True, message will become final message's body without
+                  checking types)
+                - `embed`: (keyword-only) force creating new
+                  MultiplexerMessage, don't check `message' type
+                - `multiplexer`: (keyword-only) [ ONE | ALL | Connwrap ] -- use
+                  specific (set of) MX, default = ONE
+                - `flush`: (keyword-only) wait until message is passed to the
+                  socket layer (kernel)
+                - `timeout`: (keyword-only) timeout
+                - `kwargs`: will be used in construting MultiplexerMessage
+                  where `embed' or message is not instance of
+                  MultiplexerMessage
         """
         return self.__send_message(message, **kwargs)
 
-    def __send_message(self, message, embed=False, multiplexer=ONE, flush=False, timeout=DEFAULT_TIMEOUT, **kwargs):
+    def __send_message(self, message, embed=False, multiplexer=ONE,
+            flush=False, timeout=DEFAULT_TIMEOUT, **kwargs):
 
         timeout_ticker = TimeoutTicker(timeout)
 
         if embed or not isinstance(message, MultiplexerMessage):
-            mxmsg = self.new_message(message = message, **kwargs)
+            mxmsg = self.new_message(message=message, **kwargs)
         else:
             mxmsg = message
 
@@ -319,17 +352,21 @@ class Client(_mxclient.Client):
         elif isinstance(multiplexer, ConnectionWrapper):
             tracker = self.schedule_one(raw, multiplexer, timeout_ticker())
         else:
-            raise NotImplementedError, "selecting multiplexer with %r is not supported" % multiplexer
-       
+            raise NotImplementedError, \
+                    "selecting multiplexer with %r is not supported" % \
+                        multiplexer
+
         # handle flush
         if flush and tracker is not None:
             if multiplexer is Client.ALL:
-                raise NotImplementedError, "Flushing when sending to multiple peers is not implemented."
-            self.flush(tracker, timeout = timeout_ticker())
+                raise NotImplementedError, \
+                        "Flushing when sending to multiple peers is " \
+                        "not implemented."
+            self.flush(tracker, timeout=timeout_ticker())
 
         return (mxmsg.id, tracker)
 
-    def flush(self, tracker, timeout = DEFAULT_TIMEOUT):
+    def flush(self, tracker, timeout=DEFAULT_TIMEOUT):
         """ensure that message tracked by tracker is either sent or dropped"""
         super(Client, self).flush(tracker, timeout)
 
@@ -355,7 +392,7 @@ class Client(_mxclient.Client):
         # defaults
         set_if_missing(kwargs, 'id', self.random())
         set_if_missing(kwargs, 'from', self.instance_id)
-        
+
         # special handling of some values
         if 'message' in kwargs and not isinstance(kwargs['message'], str):
             kwargs['message'] = kwargs['message'].SerializeToString()
