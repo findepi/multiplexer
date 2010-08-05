632dde33fe3f651242e2c07450eefb9da79b558e
Zbyszek Szmek <zbyszek@in.waw.pl>
2010-08-05 14:09:27 +0200

    Generate version files correctly

--- src/azouk/azouk/release.h
+++
@@ -1,8 +0,0 @@
-#ifndef x551cf8916a2bcd7d8aebc8c49c468391697f71f6
-#define x551cf8916a2bcd7d8aebc8c49c468391697f71f6
-namespace azouk { namespace release {
-	const char* const version = "87cd6ed";
-	const char* const version_hash = "0bde54bdef1af6894599b49c37d835af1edaf445";
-	const char* const version_short_hash = "0bde54b";
-} /* release */ } /* azouk */
-#endif
